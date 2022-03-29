b9862e19bf4b59bf96271585877ef76642f140c8
Mathias Bjørgum <mathigb@stud.ntnu.no>
2022-03-29 19:53:45 +0200

    made some progress with the right dataset

--- preprocessing/cleaning_functions.py
+++ preprocessing/cleaning_functions.py
@@ -1,24 +1,14 @@
-import pandas as pd
+import nltk
+import re
 
-def clean_stock_data(stock_df: pd.DataFrame) -> pd.DataFrame:
-    '''Adds labels for degree of change in the stock price, based on `Open` and `Close` columns.'''
-    stock_df["change"] = (stock_df["Close"] - stock_df["Open"]) / stock_df["Open"]
-    stock_df["change_cat"] = pd.cut(x = stock_df["change"], bins = [-0.2, -0.02, 0, 0.02, 0.2], labels = [0, 1, 2, 3])
-    stock_df["Date"] = pd.to_datetime(stock_df["Date"])
-    stock_df = stock_df.drop(labels = ["Open", "High", "Low", "Close", "Volume", "Adj Close"], axis=1)
-    return stock_df
+def clean_data(review: str) -> str:
+    '''Removes digits and stopwords from a string'''
+    stopwords = nltk.corpus.stopwords.words("english")
+    review = re.sub(r'\d+', ' ', review)
 
-def clean_news_data(news_df: pd.DataFrame) -> pd.DataFrame:
-    '''Combines all news data headlines with the same date, and adds it all to one column.'''
-    returning_df = news_df.groupby("Date")
-    returning_df = returning_df["News"].apply(list)
-    returning_df = returning_df.reset_index()
-    returning_df["Date"] = pd.to_datetime(returning_df["Date"])
-    return returning_df
+    review = review.split()
+    review = " ".join([word for word in review if not word in stopwords])
 
-def clean_and_combine(stock_df: pd.DataFrame, news_df: pd.DataFrame) -> pd.DataFrame:
-    '''Cleans and combines a given stock dataframe and news dataframe'''
-    stock_df = clean_stock_data(stock_df)
-    news_df = clean_news_data(news_df)
+    return review
 
-    return pd.merge(stock_df, news_df, how="left", on="Date")
+print("hei")
